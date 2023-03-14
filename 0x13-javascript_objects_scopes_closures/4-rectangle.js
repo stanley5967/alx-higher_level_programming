19742d9c9470fa251d4f1be3a54358aff911d0f4
Obinugwu Stanley <chukwumastanley76@gmail.com>
2023-03-14 11:55:57 -0700

    Write a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,3 +17,16 @@ class Rectangle {
     }
   }
 
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
